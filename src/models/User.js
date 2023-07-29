8cf955290e28c64157a3e517757b8a5644f870c5
gustavomorais <gustavomorais.dev@gmail.com>
2023-07-29 14:52:24 -0300

    completa o requisito 2 que cria o model do user

---
+++ src/models/User.js
@@ -0,0 +1,24 @@
+/**
+* @param {import('sequelize').Sequelize} sequelize
+* @param {import('sequelize').DataTypes} DataTypes
+*/
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    tableName: 'users',
+    underscored: true,
+  });
+
+  return User;
+}
