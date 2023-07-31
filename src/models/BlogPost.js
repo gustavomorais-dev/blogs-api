4876bae9a7bb4702e662a80c3783ce46cdea8c9b
gustavomorais <gustavomorais.dev@gmail.com>
2023-07-31 15:49:01 -0300

    completa o requisito 10 que cria a model do blogpost

---
+++ src/models/BlogPost.js
@@ -0,0 +1,48 @@
+/**
+* @param {import('sequelize').Sequelize} sequelize
+* @param {import('sequelize').DataTypes} DataTypes
+*/
+
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    published: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    updated: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  }, {
+    tableName: 'blog_posts',
+    timestamps: true,
+    createdAt: 'published',
+    updatedAt: 'updated',
+    underscored: true,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User,
+      { foreignKey: 'userId', as: 'user' })
+  }
+
+  return BlogPost;
+};
--- src/models/User.js
+++ src/models/User.js
@@ -20,5 +20,10 @@ module.exports = (sequelize, DataTypes) => {
     underscored: true,
   });
 
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost,
+      { foreignKey: 'userId', as: 'blogPosts' })
+  }
+
   return User;
-}
+};
