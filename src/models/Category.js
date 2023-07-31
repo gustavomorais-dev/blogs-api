fb717f807829a23a9284f2a296c33b383ced4e2f
gustavomorais <gustavomorais.dev@gmail.com>
2023-07-31 15:27:56 -0300

    completa o requisito 7 que cria a model do category

---
+++ src/models/Category.js
@@ -0,0 +1,24 @@
+/**
+* @param {import('sequelize').Sequelize} sequelize
+* @param {import('sequelize').DataTypes} DataTypes
+*/
+
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+    tableName: 'categories',
+    timestamps: false,
+  });
+
+  return Category;
+}
